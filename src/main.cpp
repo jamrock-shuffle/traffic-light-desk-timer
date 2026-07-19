304dc5d85d2c7abf6e4e92558db0394613404158
jamrock-shuffle <209611220+jamrock-shuffle@users.noreply.github.com>
2026-07-19 20:04:13 +0800

    Add stopwatch + refactor code

--- src/main.cpp
+++ src/main.cpp
@@ -13,35 +13,69 @@ int redButtonValue;
 int yellowButtonValue;
 int greenButtonValue;
 int mode;
+bool stopwatchActive = false;
+int timeSession = 0;
+int timeToday = 0;
+int prevSecond = -1;
 
 const char* months[] = {"ERR", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+void displayClock() {
+    lcd.setCursor(3,1);
+    
+    if(now.day() < 10) lcd.print('0'); // print day
+    lcd.print(now.day());
+    lcd.print(' ');
+            
+    lcd.print(months[now.month()]); // print month
+    lcd.print(' ');
+
+    lcd.print(now.year() % 100); // print year
+            
+    lcd.setCursor(5,0); // print time
+    if(now.hour() < 10) lcd.print('0');
+    lcd.print(now.hour());
+    lcd.print(':');
+    if(now.minute() < 10) lcd.print('0');
+    lcd.print(now.minute());
+}
+
+void displaySession() {
+    int hours = timeSession/3600;
+    int minutes = (timeSession%3600) / 60;
+    int seconds = timeSession%60;
+
+    if (hours < 10) lcd.print('0');
+    lcd.print(hours);
+    lcd.print(':');
+    if (minutes < 10) lcd.print('0');
+    lcd.print(minutes);
+    lcd.print(':');
+    if (seconds < 10) lcd.print('0');
+    lcd.print(seconds);
+}
+
+void stopwatch() {
+    if (stopwatchActive) {
+        if (now.second() != prevSecond) {
+            timeSession++;
+            timeToday++;
+            prevSecond = now.second();
+        }
+    } else timeSession = 0;
+}
 
 void displayMode() {
     switch (mode) {
         case 0:
-        lcd.setCursor(3,1);
-            if(now.day() < 10) lcd.print('0'); // print day
-            lcd.print(now.day(), DEC);
-            lcd.print(' ');
-            
-            lcd.print(months[now.month()]); // print month
-            lcd.print(' ');
-
-            lcd.print(now.year() % 100, DEC); // print year
-            
-            lcd.setCursor(5,0); // print time
-            if(now.hour() < 10) lcd.print('0');
-            lcd.print(now.hour(), DEC);
-            lcd.print(':');
-            if(now.minute() < 10) lcd.print('0');
-            lcd.print(now.minute(), DEC);
+            displayClock();
             break;
 
         case 1:
             lcd.setCursor(0,0);
             lcd.print("current session"); // time accumulated this session (essentially a stopwatch)
             lcd.setCursor(4,1);
-            lcd.print("00:00:00");
+            displaySession();
             break;
 
         case 2: 
@@ -60,38 +94,35 @@ void displayMode() {
     }
 }
 
-void cycleMode() { /* advances mode when yellow button is pressed, 
-    rolling over when mode 2 is reached
-    TO-DO: make this conditional on device not being in [EXAM MODE] - [EXAM MODE] should 
-    temporarily override all other buttons' functions*/
-    if (yellowButtonValue == LOW && mode < 2) {
-        lcd.clear();
-        mode += 1;
-        delay(200);
-    } else if (yellowButtonValue == LOW) {
-        lcd.clear();
-        mode = 0;
-        delay(200);
+void listenForButtons() {
+    static unsigned long lastPressed;
+
+    if (millis() - lastPressed > 150) { // button debouncing
+
+        if (redButtonValue == LOW) { // forces exam mode
+            lcd.clear();
+            mode = 3;
+            lastPressed = millis();
+        }
+
+        if (yellowButtonValue == LOW && mode < 2) { // advances mode when yellow button is pressed,
+            lcd.clear(); // rolling over when mode 2 is reached
+            mode += 1;
+            lastPressed = millis();
+        } else if (yellowButtonValue == LOW) {
+            lcd.clear();
+            mode = 0;
+            lastPressed = millis();
+        }
+
+        if (greenButtonValue == LOW) { // forces mode 1
+            lcd.clear();
+            mode = 1;
+            stopwatchActive = !stopwatchActive; // toggles stopwatch
+            lastPressed = millis();
+        }
     }
 }
-
-void listenForExamMode() { // forces [EXAM MODE] when red button is pressed
-    if (redButtonValue == LOW) {
-        lcd.clear();
-        mode = 3;
-        delay(200);
-    }
-}
-
-void listenForActiveFocusMode() { /* forces [ACTIVE FOCUS] when green button is pressed 
-    TO-DO: start stopwatch at the same time*/
-    if (greenButtonValue == LOW) {
-        lcd.clear();
-        mode = 1;
-        delay(200);
-    }
-}
-
 
 void setup() {
     Serial.begin(9600);
@@ -127,8 +158,7 @@ void loop() {
     yellowButtonValue = digitalRead(yellowButtonPin);
     greenButtonValue = digitalRead(greenButtonPin);
 
-    cycleMode();
-    listenForExamMode();
-    listenForActiveFocusMode();
+    listenForButtons();
+    stopwatch();
     displayMode();
 }
